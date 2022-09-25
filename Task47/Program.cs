025456c35c5e2caf4a7f630978c6567e2ac18c90
Zarina <abaevazarina@yandex.ru>
2022-09-25 21:08:02 +0300

    task47 solved

---
+++ Task47/Program.cs
@@ -0,0 +1,28 @@
+﻿double [,] GenerateArray(int rows, int columns)
+{
+    double [,] array = new double [3,4];
+
+    for (int  i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Math.Round(new Random().NextDouble() * (20 + 20) - 20, 1);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(double [,] myArray)
+{
+     for (int  i = 0; i < myArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < myArray.GetLength(1); j++)
+        {
+            Console.Write($"{myArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(GenerateArray(3,4));
